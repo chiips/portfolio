e56adceb57c9ecf94385be8d689d1100facde7e3
chiips <galanocp@gmail.com>
2019-10-08 19:46:09 -0400

    added gzip compression

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  configureWebpack: {
+    devtool: 'source-map',
+    plugins: [
+      new CompressionPlugin({ cache: true })
+    ]
+  }
+};
