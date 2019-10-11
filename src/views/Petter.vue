d427883b920268df424472caf7ed0738759b4a00
chiips <galanocp@gmail.com>
2019-10-11 14:14:20 -0400

    updates for petter

--- src/views/Petter.vue
+++ src/views/Petter.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1 class="is-size-3">Petter</h1>
+    <a href="https://galano.co" target="_blank">Website</a> |
     <a href="https://github.com/chiips/snippets" target="_blank">Github</a>
     <br>
     <br>
@@ -11,14 +12,14 @@
       <p><b>Back-end: </b>RESTful JSON API written in <b>Go</b>.</p>
       <p><b>Front-end: </b>Single-Page Application created with <b>Vue.js</b> and <b>Vuex</b>.</p>
       <p><b>Data: </b>Resource storage in <b>PostgreSQL</b> and session storage in <b>Redis</b>.</p>
-      <p><b>Deployment: </b>Served with <b>Nginx</b>.</p>
+      <p><b>Deployment: </b>Deployed with <b>Nginx</b> on <b>Ubuntu</b> server on <b>DigitalOcean</b>.</p>
       <br>
       <p><b>Note: </b> The Github link directs to a web snippets repository highlighting the structure of the application and key code examples.</p>
       <br>
     </div>
     </div>
 
-    <a href="https://github.com/chiips/snippets" target="_blank"><img src="../assets/petter.png"/></a>
+    <a href="https://galano.co" target="_blank"><img src="../assets/petter.png"/></a>
 
   </div>
 </template>
