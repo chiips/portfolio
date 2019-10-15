fd1f7d61f62f5768be8f95301d1998aa7a7785f6
chiips <galanocp@gmail.com>
2019-10-15 11:03:27 -0400

    fixed description

--- src/views/SpellCaster.vue
+++ src/views/SpellCaster.vue
@@ -6,7 +6,7 @@
     <br>
     <div class="columns">
     <div class="column is-half is-offset-one-quarter has-text-left">
-      <p><b>Description: </b>SpellCaster is social media for wizards and witches. Share your spells and find new ones.</p>
+      <p><b>Description: </b>SpellCaster is social media for witches and wizards. Share your spells and find new ones.</p>
       <br>
       <p><b>Back-end: </b>Multi-Page Application written in <b>Go</b>.</p>
       <p><b>Front-end: </b>Web pages styled with <b>HTML</b>, <b>CSS</b>, <b>Javascript</b>, and <b>jQuery</b>.</p>
